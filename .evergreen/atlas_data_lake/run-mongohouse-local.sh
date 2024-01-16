0422b779a457c3774d3ae95302cb84b5b42fc980
Kevin Albertson <kevin.albertson@mongodb.com>
2024-01-16 08:25:52 -0500

    run `handle-paths.sh` before changing working directory
    Fixes invocation of `run-mongohouse-local.sh`

--- .evergreen/atlas_data_lake/run-mongohouse-local.sh
+++ .evergreen/atlas_data_lake/run-mongohouse-local.sh
@@ -5,12 +5,12 @@
 # There is no corresponding 'shutdown' script; this project relies
 # on Evergreen to terminate processes and clean up when tasks end.
 
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+. $SCRIPT_DIR/../handle-paths.sh
 cd mongohouse
 if [ "Windows_NT" = "$OS" ]; then
   export MONGOHOUSE_MQLRUN="$(cygpath -m "$(pwd)")/artifacts/mqlrun.exe"
 else
   export MONGOHOUSE_MQLRUN=`pwd`/artifacts/mqlrun
 fi;
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
-. $SCRIPT_DIR/../handle-paths.sh
 ./artifacts/mongohoused --config ${DRIVERS_TOOLS}/.evergreen/atlas_data_lake/config.yml
